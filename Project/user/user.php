105afe34027a56517c3e49a924dc248492b2f691
SanjanaMahesh3 <76697358+SanjanaMahesh3@users.noreply.github.com>
2022-12-02 23:33:26 +0530

    Add files via upload

---
+++ Project/user/user.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+  <body>
+    <link href = "style.css" type = "text/css" rel = "stylesheet" />
+    <h1>Welcome User</h1>
+    <div class="div2">
+    <button class="b1" onclick="document.location.href='new_user/new_user.php'">New User</button>
+    <button class="b2" onclick="document.location.href='exis_user/exis_user.php'">Existing User</button>
