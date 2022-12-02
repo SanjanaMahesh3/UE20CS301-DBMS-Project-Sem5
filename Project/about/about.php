8a9f974dd08609530a8ef887be74a230f7233a9d
SanjanaMahesh3 <76697358+SanjanaMahesh3@users.noreply.github.com>
2022-12-02 23:28:47 +0530

    Add files via upload

---
+++ Project/about/about.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+  <body>
+    <link href = "style.css" type = "text/css" rel = "stylesheet" />
+    <h1 class="head1">About us</h1>
+    <h4 class="new1">
+      Health insurance
+      ****Add content**** 
+</h4>
